2c561fc5aa8bd90656e231b469b103d298eb706c
stas <bubub@mail.ru>
2017-06-25 23:14:35 +0300

    Comit JavaRush

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0822/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.task08.task0822;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+/* 
+Минимальное из N чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        List<Integer> integerList = getIntegerList();
+        System.out.println(getMinimum(integerList));
+    }
+
+    public static int getMinimum(List<Integer> array) {
+        int min;
+        min = array.get(0);
+        for (int i = 0; i < array.size(); i++) {
+            if (min > array.get(i)){
+                min = array.get(i);
+            }
+        }
+        return min;
+    }
+
+    public static List<Integer> getIntegerList() throws IOException {
+        ArrayList<Integer> list = new ArrayList<>();
+        int strokenumber;
+        Scanner reader = new Scanner(System.in);
+        strokenumber = reader.nextInt();
+
+        for (int i = 0; i < strokenumber; i++) {
+            list.add(reader.nextInt());
+        }
+
+        return list;
+    }
+}
